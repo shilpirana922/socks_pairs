27d7bab9ce1f928a2063d1e44ad6335753b9f967
shilpirana922 <ranashilpi922@gmail.com>
2020-10-18 18:55:51 +0530

    adding comments

--- script.js
+++ script.js
@@ -17,33 +17,37 @@ $("#sign-form").submit((e)=>{
  $("#result").html(`<b>Total number of matching pairs of socks: ${allPairs}</b>`);
  
 })
-
+// find all pairs of socks
 function findPairs(size, arr){
 
+    // if size is zero then return zero pairs
    if(size==0 || arr.length==0)
     return 0;
 
+    // map for finding the count of socks for a unique color 
    let socksUniqueMap = new Map();
 
     for(var i=0;i<arr.length;i++){
-    
+    // if socks already present then increase the count
+
     if(socksUniqueMap.has(arr[i])){
         socksUniqueMap.set(arr[i], socksUniqueMap.get(arr[i])+1)
     }
     else{
+        //otherwises add the socks
         socksUniqueMap.set(arr[i],1);
     }
 }
 
-
+// get the array of all values
 var eachSocksCount =  socksUniqueMap.values();
 
 let totalPairs = 0;
-
+// count the pairs by dividin them for two
 for(var ele of eachSocksCount){
     totalPairs +=parseInt(ele/2);
 }
-
+// return total number of pairs
 return totalPairs;
 } 
 
